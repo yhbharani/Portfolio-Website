28e0a3286a546842a33bf1705f43c66695717960
yhbharani <yash.h.bharani@gmail.com>
2020-05-05 16:39:18 +0530

    contact form validation added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup } from '@angular/forms'
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  name: string='';
-  email: string='';
-  message: string='';
+  Name: string='';
+  Email: string='';
+  Message: string='';
   phone: number;
+  form: FormGroup;
   
   constructor() { }
 
@@ -18,8 +19,12 @@ export class ContactComponent implements OnInit {
   }
 
   processForm() {
-    const allInfo = `My name is ${this.name}. My email is ${this.email}.Phone No: ${this.phone}. My message is ${this.message}`;
+    const allInfo = `My name is ${this.Name}. My email is ${this.Email}.Phone No: ${this.phone}. My message is ${this.Message}`;
     alert('Message is currently Disabled :( ........................ Please Email me at yash.h.bharani@gmail.com'); 
+  }
+
+  notValid(){
+    alert('Form Not Valid !! Please Check the fields')
   }
 
 }
